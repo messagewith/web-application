ac88c6f00b8cc59a1f8032bc2538593ff2b547a7
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2021-05-13 14:24:41 +0200

    Added support for dynamic values in the text function with useTranslation (i18n)

--- packages/i18n/src/hooks/useTranslation.ts
+++ packages/i18n/src/hooks/useTranslation.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/restrict-template-expressions */
 
 import { useContext } from "react";
 import { I18nContext } from "../I18nProvider";
@@ -6,7 +6,10 @@ import { I18nContext } from "../I18nProvider";
 export const useTranslation = () => {
   const { texts, changeLanguage, isoCode } = useContext(I18nContext);
 
-  const text = (id: string): string | undefined => {
+  const text = (
+    id: string,
+    values?: { [key: string]: string }
+  ): string | undefined => {
     const keys = id.split(".");
 
     // @ts-ignore
@@ -27,6 +30,14 @@ export const useTranslation = () => {
         property = property?.[key];
       }
     });
+
+    if (typeof property === "string" && values) {
+      Object.entries(values).forEach(([key, value]) => {
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        property = property.replaceAll(`{{${key}}}`, value);
+      });
+    }
 
     return property;
   };
