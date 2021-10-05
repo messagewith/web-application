6422dab2ac7499494169a2c050999eaf4d747d97
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2021-10-05 11:12:26 +0200

    core-ui: Add exports and update version

--- packages/core-ui/src/index.ts
+++ packages/core-ui/src/index.ts
@@ -1,10 +1,16 @@
-// Components
-
+// Components atoms
+export * from "./components/atoms/AccountProfilePicture/AccountProfilePicture";
 export * from "./components/atoms/Button/Button";
-export * from "./components/atoms/InDevelopmentInfo/InDevelopment";
-export * from "./components/atoms/Logo/Logo";
-export * from "./components/atoms/Input/Input";
 export * from "./components/atoms/ChangeLanguage/ChangeLanguage";
+export * from "./components/atoms/IconButton/IconButton";
+export * from "./components/atoms/InDevelopmentInfo/InDevelopment";
+export * from "./components/atoms/Input/Input";
+export * from "./components/atoms/Logo/Logo";
+export * from "./components/atoms/Textarea/Textarea";
+
+// Components molecules
+export * from "./components/molecules/ChatTextarea/ChatTextarea";
+export * from "./components/molecules/ChatUser/ChatUser";
 export * from "./components/molecules/FunctionNotAvailable/FunctionNotAvailable";
 
 // Theme
