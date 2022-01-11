5012db71c85d73ef24dcab538634504cb30fcafe
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2022-01-11 17:13:29 +0100

    feat(server): Add support for https in production

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from "@nestjs/common";
 import { join } from "path";
 import { GraphQLModule } from "@nestjs/graphql";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { SessionsModule } from "./sessions/sessions.module";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MESSAGEWITH_DATABASE_URI),
+    ConfigModule.forRoot({
+      envFilePath: join(process.cwd(), `.env.${process.env.NODE_ENV}`),
+    }),
+    MongooseModule.forRoot(process.env.MESSAGEWITH_DATABASE_URI, {
+      tlsCAFile:
+        process.env.NODE_ENV === "production"
+          ? join(process.cwd(), "certs", "ca-certificate.crt")
+          : undefined,
+      dbName: "messagewith",
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
     }),
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
+import * as dotenv from "dotenv";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
+import * as fs from "fs";
 import * as cors from "cors";
+import * as path from "path";
 import { AppModule } from "./app.module";
 
+dotenv.config({
+  path: path.join(process.cwd(), `.env.${process.env.NODE_ENV}`),
+});
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            key: fs.readFileSync(path.join(process.cwd(), "certs", "key.pem")),
+            cert: fs.readFileSync(
+              path.join(process.cwd(), "certs", "cert.pem")
+            ),
+            ca: fs.readFileSync(path.join(process.cwd(), "certs", "chain.pem")),
+          }
+        : undefined,
+  });
 
   app.use(cookieParser());
   app.use(cors({ credentials: true, origin: true }));
